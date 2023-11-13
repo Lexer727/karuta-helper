6a277278defbde157d594d66e3aa2f445ee682be
Lexer727 <a.staub1307@gmail.com>
2023-11-13 20:15:51 +0100

    dropdata+wl

---
+++ assets/addWlToCSV.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+wlfile = 'wishlistv2.txt'
+csvfile = 'data.csv'
+
+df = pd.read_csv(csvfile)
+
+with open(wlfile, 'r', encoding='utf-8-sig') as f:
+    for line in f:
+        entry = [x.replace('\u2764\ufe0f','').strip() for x in line.split('·')]
+        entry.pop(0)
+        entry[0] = '❤️ ' + entry[0]
+        entry[2].replace('.','')
+
+        df.loc[df['Character'].str.contains(entry[1]) & df['Series'].str.contains(entry[2]), 'Wishlist'] = entry[0]
+    
+df.to_csv('datawl.csv')  
--- assets/txtToJson.py
+++ assets/txtToJson.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 filename = 'wishlistv2.txt'
 
